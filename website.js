33e4cc9cccef04675bc87eea44bf85cca11092ad
Chrisy <chrisyhaigh@gmail.com>
2023-06-27 14:35:35 +0100

    updated Contact with JS for label. Replaced cards rather than carousel for Portfolio. Animated intro section

--- website.js
+++ website.js
@@ -1,3 +1,15 @@
+
+//label input in Contact form
+function handleInput(element) {
+    const label = element.previousElementSibling;
+
+    if (element.value !== '') {
+        label.classList.add('hidden');
+    } else {
+        label.classList.remove('hidden');
+    }
+};
+
 //preloader
 $(window).on('load', function () {
     if ($('#preloader').length) {
