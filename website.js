210011385d7d0f38ce1838533ce8c6d45e8ce2e7
Chrisy <chrisyhaigh@gmail.com>
2023-08-22 11:57:42 +0100

    Updated 22/8

--- website.js
+++ website.js
@@ -1,4 +1,30 @@
+function skillsClick() {
+    const logoContainer = document.querySelector('.skills-container .logo-container');
+  
+    if (logoContainer.style.display === 'none') {
+      logoContainer.style.display = 'flex';
+    } else {
+      logoContainer.style.display = 'none';
+    }
+  }
+  
+const skillsButton = document.getElementById('skills-button');
+skillsButton.addEventListener('click', skillsClick);
 
+function experienceClick() {
+    const experienceContainer = document.querySelector('#experience .experience-container');
+
+    if (experienceContainer.style.display === 'none') {
+        experienceContainer.style.display = 'block';
+    } else {
+        experienceContainer.style.display = 'none';
+    }
+}
+
+const experienceButton = document.getElementById('experience-button');
+experienceButton.addEventListener('click', experienceClick);
+
+  
 //label input in Contact form
 function handleInput(element) {
     const label = element.previousElementSibling;
