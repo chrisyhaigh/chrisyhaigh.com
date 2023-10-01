3e00940bb475671dd178c474ef4f2bb698a505e2
Chrisy <chrisyhaigh@gmail.com>
2023-10-01 22:35:07 +0100

    Made various changes to layout of the site in prep for completing showcase

---
+++ JS/website.js
@@ -0,0 +1,11 @@
+
+
+//preloader
+$(window).on('load', function () {
+    if ($('#preloader').length) {
+        $('#preloader').delay(1000).fadeOut('slow', function () {
+            $(this).remove();
+    });
+    }
+});
+
